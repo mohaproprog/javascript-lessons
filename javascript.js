1cc1de0ae865c39a67a6795d1515dc5986a62d22
mohaproprog <mohaproprog@gmail.com>
2025-07-05 23:25:11 +0100

    js lessons

---
+++ javascript.js
@@ -0,0 +1,43 @@
+// style
+
+// let h1 = document.querySelector("h1");
+// h1.style.color= "blue";
+// h1.style.fontSize = "38px";
+// h1.style.textAlign = "center";
+
+// let pra = document.querySelector ("p");
+// pra.style.margin = "50px";
+// pra.style.marginTop = "0";
+// pra.style.fontWeight = "600";
+// pra.style.fontSize = "20px";
+// pra.style.border = "1px solid green";
+// pra.style.padding = "20px";
+// pra.style.borderRadius ="10px 30px";
+// pra.style.background = "green";
+// pra.style.lineHeight = "2rem";
+// pra.style.position ="relative";
+// pra.style.top = "50px";
+
+
+// events
+let button = document.querySelector("p");
+// button.addEventListener("click",()=>{
+//     console.log("clicked");
+// })
+
+// button.addEventListener("mouseover",()=>{
+//     console.log("clicked");
+// })
+
+button.addEventListener("click",()=>{
+    alert("massage");
+})
+button.addEventListener("mouseover"()=>{
+
+}0
+
+
+
+
+
+
